839e27d4b6ccb74c5e3fec3e92a6b63243aac7a2
Tim <mohamud.hussein@student.moringaschool.com>
2023-06-17 20:31:55 +0300

    first assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+// 1) Initialize the cats array with initial values
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  
